ab6f1e8045a7cdc2dd55f578fb4226aff63d356c
Naveen Hegde <nshegdecse@gmail.com>
2024-07-25 12:50:11 +0530

    Entity classes created

---
+++ src/main/java/com/nobel/NobelHotel/NobelHotelApplication.java
@@ -0,0 +1,13 @@
+package com.nobel.NobelHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NobelHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NobelHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nobel/NobelHotel/NobelHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nobel.NobelHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NobelHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
