9fea3f7edd8908de1293d3c40f3a886ce910829a
Naveen Hegde <nshegdecse@gmail.com>
2024-07-27 17:21:13 +0530

    Security related classes added

---
+++ src/main/java/com/nobel/NobelHotel/service/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.nobel.NobelHotel.service;
+
+import com.nobel.NobelHotel.exception.OurException;
+import com.nobel.NobelHotel.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new OurException("Username/Email not Found"));
+    }
+}
